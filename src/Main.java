79db3ef15b4168f12423227eb590bd9e77e38037
Ivanov Peter <peterivan5555@gmail.com>
2022-04-18 15:41:57 +0700

    CreateMenu

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Objects;
+
 public class Main extends Application{
     public static void main(String[] args) {		
         Application.launch(args);	
@@ -13,7 +15,7 @@ public class Main extends Application{
      
     @Override
     public void start(Stage primaryStage) throws Exception {
-        AnchorPane root = FXMLLoader.load(getClass().getResource("\\resources\\splash.fxml"));
+        AnchorPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("\\resources\\splash.fxml")));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.UNDECORATED);
