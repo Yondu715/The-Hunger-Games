15a4accc78da3df1115b182e5f46caa4738bd368
Ivanov Peter <peterivan5555@gmail.com>
2022-04-18 15:53:02 +0700

    CreateMenu 0.01

--- src/run.bat
+++ src/run.bat
@@ -1,1 +1,2 @@
+java --module-path .\Libs\javafx\lib --add-modules=javafx.controls,javafx.fxml --class-path "..;.;.\src" src.MainMenu
 java --module-path .\Libs\javafx\lib --add-modules=javafx.controls,javafx.fxml --class-path "..;.;.\src" src.Main
